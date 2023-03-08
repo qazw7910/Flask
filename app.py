71e4150210921c5856db9ee84957485f37362a3f
陳柏志 <okmijn7980@gmail.com>
2023-03-08 17:43:46 +0800

    move the home.html to templates

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
+@app.route("/")
+def home():
+    return render_template("home.html")
 @app.route("/expression/")
 def render_expression():
     # interpolation
