01a73b3122c4a7369efa67084e9c066dcc069291
陳柏志 <okmijn7980@gmail.com>
2023-03-08 18:22:19 +0800

    setting up microblog_with_flask

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods = ["GET", "POST"])
 def home():
+    if request.method == "POST":
+        entry_content = request.form.get("content")
+        print(entry_content)
     return render_template("home.html")
 @app.route("/expression/")
 def render_expression():
